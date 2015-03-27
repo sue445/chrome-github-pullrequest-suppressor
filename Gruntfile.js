d88ffc4b83ada954df340aa7013d68dccd1abfd9
sue445 <sue445@sue445.net>
2015-03-27 20:08:51 +0900

    npm install  --save-dev

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function (grunt) {
+  var manifest = grunt.file.readJSON("manifest.json");
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    'chrome-extension': {
+      options: {
+        name: "chrome-github-upstream-pullrequest-suppressor",
+        version: manifest.version,
+        id: "aehgkgapfagaljikampcebpacdcpkbfc",
+        chrome: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        clean: true,
+        buildDir: 'build',
+        resources: [
+          "css/**",
+          "fonts/**",
+          "img/**",
+          "lib/**",
+          "src/**",
+          "*.html",
+          "LICENSE",
+          "manifest.json",
+          "README.md"
+        ]
+      }
+    },
+    gittag: {
+      new_version: {
+        options: {
+          tag: manifest.version,
+          message: "release v" + manifest.version
+        }
+      }
+    },
+    gitpush: {
+      tag: {
+        options: {
+          tags: true
+        }
+      },
+      master: {
+        options: {
+          branch: "master"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-chrome-compile');
+  grunt.loadNpmTasks('grunt-git');
+
+  grunt.registerTask('default', ["chrome-extension", "gittag:new_version", "gitpush:tag", "gitpush:master"]);
+};
